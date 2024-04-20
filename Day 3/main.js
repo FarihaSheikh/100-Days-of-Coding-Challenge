c93af257892380d8fc41e84506ecfbeb8b9572bd
Fariha <farihasheikh529@gmail.com>
2024-04-20 10:45:19 -0700

    Day3 commit

---
+++ Day 3/main.js
@@ -0,0 +1,30 @@
+// **********************Day 3 Challenge **********************************
+//                       Question 7:
+//  Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+//  Enclose your operations in print statements.
+let num1 = 5;
+let num2 = 3;
+console.log("Addition:", num1 + num2);
+let num3 = 10;
+let num4 = 2;
+console.log("Subtraction: ", num3 - num4);
+let num5 = 4;
+let num6 = 2;
+console.log("Multiplication: ", num5 * num6);
+let num7 = 16;
+let num8 = 2;
+console.log("Division: ", num7 / num8);
+//                          Question 8: 
+// You should create four lines that look like this:
+// console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+//                          Question 9: 
+// Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number.
+//  Print that message.
+let favouriteNumber = 18;
+console.log("My favourite number is " + favouriteNumber);
+export {};
---
+++ Day 3/main.ts
@@ -0,0 +1,47 @@
+// ********************* Day 3 Challenge **********************************
+
+//                       Question 7:
+//  Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+//  Enclose your operations in print statements.
+
+let num1: number = 5;
+let num2: number = 3;
+
+console.log("Addition:",num1 + num2);
+
+let num3: number = 10;
+let num4: number = 2;
+
+console.log("Subtraction: ",num3 - num4);
+
+let num5: number = 4;
+let num6: number =2;
+
+console.log("Multiplication: ",num5 * num6);
+
+let num7: number = 16;
+let num8: number = 2;
+console.log("Division: ",num7 / num8);
+
+
+//                          Question 8: 
+// You should create four lines that look like this:
+// console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+
+
+console.log(5 + 3);
+console.log(10-2);
+console.log(4*2);
+console.log(16/2);
+
+
+//                          Question 9: 
+// Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number.
+//  Print that message.
+
+let favouriteNumber: number = 18;
+
+console.log("My favourite number is " + favouriteNumber);
+
+

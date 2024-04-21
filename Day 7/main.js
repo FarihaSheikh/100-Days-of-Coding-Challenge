8d09408f2916f3e797212a601332b3378ac5d2b6
Fariha <farihasheikh529@gmail.com>
2024-04-21 09:15:37 -0700

    Day7 commit

---
+++ Day 7/main.js
@@ -0,0 +1,22 @@
+// ******************** Day 7 Challenge ************************
+//                      Question 19:
+// Dinner Guests: Use one of your programs from Exercises 14 through 18 to print a message indicating the number of
+// people you are inviting to dinner.
+let guestList = ["Rozina", "Mahnoor", "Shagufta", "Rabiya"];
+console.log(`I am inviting ${guestList.length} people to dinner`);
+//                      Question 20:
+//Think of something you could store in an array. For example, you could make a list of mountains, rivers, countries,
+// cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+let countries = ["USA", "Paris", "Singapore", "Malaysia"];
+console.log(`Countries i would like to visit ${countries}`);
+//                      Question 21:
+// Think of something you could store in a TypeScript Object. Write a program that creates Objects containing 
+//these items.
+let car = {
+    name: "Toyota",
+    model: "Corolla",
+    year: 2023,
+    price: 200000000
+};
+console.log(`Car name: ${car.name} of model: ${car.model} of year ${car.year}, price: ${car.price}`);
+export {};
